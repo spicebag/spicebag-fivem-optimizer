b89ebc40ac0d6d8105a6b04c539de20740bc2ed5
Spicebag <94927382+spicebag@users.noreply.github.com>
2022-03-08 22:42:33 +0000

    Added some stuff that killed demanding programs
    Added a list of demanding programs to close before you open the game, and added a power plan that is optional to increase performance.

---
+++ spicebag-fivem-optimizer.bat
@@ -0,0 +1,58 @@
+@echo off
+title https://github.com/spicebag
+color 6
+
+echo 
+echo Press Any Key to optimize FiveM by deleting the cache and closing down some resource intensive 
+pause >nul
+cls
+
+echo Clearing FiveM Cache...
+TIMEOUT /T 1
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\browser"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\db"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\priv"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\servers"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\subprocess"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\cache\unconfirmed"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\crashometry"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\launcher_skip_mtl2"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\session"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\plugins"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\mods"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\logs"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\crashes"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\caches.XML"
+del /s /q /f "%LocalAppData%\FiveM\FiveM.app\adhesive.dll"
+
+
+
+echo Optimizing FiveM...
+TIMEOUT /T 1
+taskkill /f /im GTAVLauncher.exe
+wmic process where name="FiveM.exe" CALL setpriority 128
+wmic process where name="FiveM_b2189_GTAProcess.exe" CALL setpriority 128
+taskkill /f /im wmpnetwk.exe.exe
+taskkill /f /im OneDrive.exe
+taskkill /f /im speedfan.exe
+taskkill /f /im TeamWiever_Service.exe
+taskkill /f /im Teamviewer.exe
+taskkill /f /im TeamViewer_Service.exe
+taskkill /f /im opera.exe
+taskkill /f /im java.exed
+taskkill /f /im Cortana.exe
+taskkill /f /im Chrome.exe
+taskkill /f /im 
+
+echo 
+echo Press Any Key to change your power-plan to increase performance, close the program if you do not want it. (Highly suggested for laptops, and Recommended for desktops.)
+pause >nul
+cls
+
+echo Changing Power Plan. . .
+TIMEOUT /T 1
+powercfg -s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+
+TIMEOUT /T 1
+color e
+echo Optimized FiveM and Cleared your cache, this script was created by spicebag, concept expanded by Spicebag, original concept by AYZNN.
